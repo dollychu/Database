40f47ba76be25c601d89e20db20d8e287e8a7a2f
dollychu <blue.sweet.soda@gmail.com>
2018-07-02 18:16:07 +0800

    Add files via upload

--- php/PrintMailContent.php
+++ php/PrintMailContent.php
@@ -1,76 +1,35 @@
 <?php  
-session_start();
 
-include "loadIcon.php";
-include "getConnectList.php";
+require_once "loadIcon.php";
+require_once "getConnectList.php";
 $icon = load_icon($_SESSION['IdUser']);
 
-foreach($result as $contact){
-  echo <<< _END
-          <div class="tab-pane fade show active" role="tabpanel" id="{$contact['ContactUser']}">
-_END;
-
-  $mails = getMailContent($contact['ContactUser']);
-  foreach($mails as $content){
+foreach($result as $mail){
+  if($mail['IdTo'] = $_SESSION['IdUser']){
     echo <<< _END
+<div class="from-them" role="tabpanel" id="{$mail['MailFrom']}">
             <div class="container-fluid row">
               <form class="col-md-9">
-                <div class="form-group row">
-                  <label for="accountName" class="col-sm-3 col-form-label">From:</label>
-                  <div class="col-sm-9">
-                    <input type="text" readonly class="form-control-plaintext" value="{$content['MailFrom']}">
-                  </div>
-                </div>
-
-                <div class="form-group">
-                  <div class="form-group row">
-                    <label for="birthdayInfo" class="col-sm-3 col-form-label">To:</label>
-                    <div class="col-sm-9">
-                      <input type="text" readonly class="form-control-plaintext" value="{$content['MailTo']}">
-                    </div>
-                  </div>
-                </div>
-
                 <div class="form-group">
                   {$mail['MailContent']}
                 </div>
-
-                <div class="form-group">
-                  <div class="form-group row">
-                    <label for="birthdayInfo" class="col-sm-3 col-form-label"></label>
-                    <div class="col-sm-9">
-                      <input type="text" readonly class="form-control-plaintext" value="{$mail['SentTime']}">
-                    </div>
-                  </div>
-                </div>
-
               </form>
-            </div><!--End of inner container-->         
+            </div><!--End of inner container-->
+          </div>
+_END;
+  }else{
+    echo <<< _END
+<div class="from-me" role="tabpanel" id="{$mail['MailFrom']}">
+            <div class="container-fluid row">
+              <form class="col-md-9">
+                <div class="form-group">
+                  {$mail['MailContent']}
+                </div>
+              </form>
+            </div><!--End of inner container-->
+          </div>
 _END;
   }
-  echo "</div>";
-}
-
-function getMailContent($otherUsr){
-  require_once "login2.php";
-  $conn = get_connection();
-  $query = "SELECT DISTINCT (SELECT Name FROM User WHERE IdUser=Mail.IdFrom) AS usrFrom, (SELECT Name FROM User WHERE IdUser=Mail.IdTo) AS usrTo, Mail.Content, Mail.CreateAt, Mail.Enabled FROM User INNER JOIN Mail WHERE (IdFrom=(SELECT IdUser FROM User WHERE NAME=$otherUsr) AND IdTo=(SELECT IdUser FROM User WHERE NAME={$_SESSION['IdUser']})) OR (IdFrom=(SELECT IdUser FROM User WHERE NAME={$_SESSION['IdUser']}) AND IdTo=(SELECT IdUser FROM User WHERE NAME=$otherUsr))";
-
-  if($result = $conn->query($query)){
-    $return_v = array();
-    while($row = $result->fetch_array()){
-      $assoc = array('MailFrom'=>$row[0],'MailTo'=>$row[1],'MailContent'=>$row[2],'SentTime'=>$row[3], 'display'=>$row[4]);
-      $return_v[] = $assoc;
-    }
-    return $return_v;
-  }
   
-  ALERT("Fail to process the query.");
-  return 0;
 }
-
-function ALERT($msg){
-  echo "<script> alert('$msg'); </script>";
-}
-
 ?>
--- php/getConnectList.php
+++ php/getConnectList.php
@@ -1,31 +1,39 @@
 <?php
 
-$result = getConnectList($_SESSION['IdUser']);
+$result = getConnect($_SESSION['IdUser']);
 
-foreach($result as $contact){
+if(empty($result)){
+  echo "<p> you don't have any mail. </p>";
+  return;
+}
+
+foreach($result as $mail){
   echo <<< _END
-    <a class="list-group-item list-group-item-action active" data-toggle="list" href="#{$contact['ContactUser']}" role="tab"> {$mail['ContactUser']} </a>
+    <a class="list-group-item list-group-item-action active" data-toggle="list" href="#{$mail['MailTo']}" role="tab"> {$mail['MailTo']} </a>
 _END;
 }
 
-function getConnectList($uid){
+function getConnect($uid){
   require_once "login2.php";
   $conn = get_connection();
-  $query = "SELECT DISTINCT User.Name FROM User INNER JOIN Mail WHERE IdUser IN (SELECT DISTINCT IdTo FROM User INNER JOIN Mail WHERE IdFrom=IdUser AND Name=$uid) OR IdUser IN (SELECT DISTINCT IdFrom FROM User INNER JOIN Mail WHERE IdTo=IdUser AND Name=$uid)";
-
+  
+  $query = "SELECT User.Name as contact, Mail.Content, Mail.IdFrom, Mail.CreateAt, Mail.IdTo, Mail.Enabled
+            FROM User INNER JOIN Mail 
+            WHERE Mail.IdFrom={$_SESSION['IdUser']} AND User.Name in(SELECT Name FROM User WHERE IdUser=Mail.IdTo) OR Mail.IdTo={$_SESSION['IdUser']} AND User.Name in(SELECT Name FROM User WHERE IdUser=Mail.IdFrom)";
+  
   if($result = $conn->query($query)){
     $return_v = array();
     while($row = $result->fetch_array()){
-      $assoc = array('ContactUser'=>$row[0]);
+      $assoc = array('MailFrom'=>$uid, 'IdTo'=>$row[4], 'MailContent'=>$row[1], 'SentDate'=>explode(' ',$row[2])[0], 'Shows'=>$row[3]);
       $return_v[] = $assoc;
     }
+    
     return $return_v;
   }
   
   ALERT("Fail to process the query.");
   return 0;
 }
-
 function ALERT($msg){
   echo "<script> alert('$msg'); </script>";
 }
