348cc5e29d36dc50ec0f50d3e7108587f48d1ab0
dollychu <blue.sweet.soda@gmail.com>
2018-07-03 05:11:41 +0800

    Add files via upload

--- php/PrintMailContent.php
+++ php/PrintMailContent.php
@@ -2,34 +2,67 @@
 
 require_once "loadIcon.php";
 require_once "getConnectList.php";
-$icon = load_icon($_SESSION['IdUser']);
+# $icon = load_icon($_SESSION['IdUser']);
 
-foreach($result as $mail){
-  if($mail['IdTo'] = $_SESSION['IdUser']){
-    echo <<< _END
-<div class="from-them" role="tabpanel" id="{$mail['MailFrom']}">
-            <div class="container-fluid row">
-              <form class="col-md-9">
-                <div class="form-group">
-                  {$mail['MailContent']}
-                </div>
-              </form>
-            </div><!--End of inner container-->
-          </div>
-_END;
+$i = 0;
+foreach($result as $contact){
+  if ($i == 0) {
+    echo "<div class='tab-pane fade show active' role='tabpanel' id='{$contact['MailFrom']}'>\n\t<section>";
   }else{
-    echo <<< _END
-<div class="from-me" role="tabpanel" id="{$mail['MailFrom']}">
-            <div class="container-fluid row">
-              <form class="col-md-9">
-                <div class="form-group">
-                  {$mail['MailContent']}
-                </div>
-              </form>
-            </div><!--End of inner container-->
-          </div>
-_END;
+    echo "<div class='tab-pane fade' role='tabpanel' id='{$contact['MailFrom']}'>\n\t\t\t<section>";
   }
+  $i ++;
   
+  $res = getContent($contact['MailFrom']);
+
+  if(empty($res)){
+    echo "<p> you don't have any message. </p>";
+    return;
+  }
+
+  foreach($res as $content){
+    #  if($mail['IdTo'] = $_SESSION['IdUser']){
+    if($content['IdTo'] == 43){
+      echo <<< _END
+              <div class="from-them">
+                <p>{$content['Content']}</p>
+              </div><!--End of message container-->
+_END;
+    }else{
+      echo <<< _END
+              <div class="from-me">
+                <p>{$content['Content']}</p>
+              </div><!--End of message container-->
+_END;
+    }
+    echo "<div class='clear'></div>";
+  }
+  echo "</section></div>\n";
 }
+
+function getContent($name){
+  
+  require_once "login2.php";
+  $conn = get_connection();
+  $name = mysqli_real_escape_string($conn, $name);
+
+  $query = "SELECT Mail.IdFrom, Mail.IdTo, Mail.Content, Mail.CreateAt, Mail.Enabled
+            FROM User INNER JOIN Mail 
+            WHERE User.Name='$name' AND ((Mail.IdFrom=43 AND Mail.IdTo=User.IdUser) OR (Mail.IdTo=43 AND Mail.IdFrom=User.IdUser));";
+    
+  if($result = $conn->query($query)){
+    $return_v = array();
+    while($row = $result->fetch_array()){
+      $assoc = array('IdFrom'=>$row[0], 'IdTo'=>$row[1], 'Content'=>$row[2], 
+                     'CreateDate'=>$row[3], 'Show'=>$row[4]);
+      $return_v[] = $assoc;
+    }
+
+    return $return_v;
+  }
+
+  ALERT("Fail to process the query. haha");
+  return 0;
+}
+
 ?>
--- php/getConnectList.php
+++ php/getConnectList.php
@@ -1,30 +1,39 @@
 <?php
 
-$result = getConnect($_SESSION['IdUser']);
+#$result = getConnect($_SESSION['IdUser']);
+$result = getConnect(43);
 
 if(empty($result)){
   echo "<p> you don't have any mail. </p>";
   return;
 }
-
+$i = 0;
 foreach($result as $mail){
-  echo <<< _END
-    <a class="list-group-item list-group-item-action active" data-toggle="list" href="#{$mail['MailTo']}" role="tab"> {$mail['MailTo']} </a>
+  if($i == 0){
+    echo <<< _END
+    <a class="list-group-item list-group-item-action active" data-toggle="list" href="#{$mail['MailFrom']}" role="tab"> {$mail['MailFrom']} </a>
 _END;
+  }else{
+    echo <<< _END
+    <a class="list-group-item list-group-item-action" data-toggle="list" href="#{$mail['MailFrom']}" role="tab"> {$mail['MailFrom']} </a>
+_END;
+  }
+  $i ++;
 }
 
 function getConnect($uid){
   require_once "login2.php";
   $conn = get_connection();
   
-  $query = "SELECT User.Name as contact, Mail.Content, Mail.IdFrom, Mail.CreateAt, Mail.IdTo, Mail.Enabled
-            FROM User INNER JOIN Mail 
-            WHERE Mail.IdFrom={$_SESSION['IdUser']} AND User.Name in(SELECT Name FROM User WHERE IdUser=Mail.IdTo) OR Mail.IdTo={$_SESSION['IdUser']} AND User.Name in(SELECT Name FROM User WHERE IdUser=Mail.IdFrom)";
+  $query = "SELECT User.Name as contact
+    FROM User INNER JOIN Mail 
+    WHERE Mail.IdFrom=43 AND User.Name in(SELECT Name FROM User WHERE IdUser=Mail.IdTo) or Mail.IdTo=43 AND User.Name in(SELECT Name FROM User WHERE IdUser=Mail.IdFrom)
+    GROUP BY User.Name;";
   
   if($result = $conn->query($query)){
     $return_v = array();
     while($row = $result->fetch_array()){
-      $assoc = array('MailFrom'=>$uid, 'IdTo'=>$row[4], 'MailContent'=>$row[1], 'SentDate'=>explode(' ',$row[2])[0], 'Shows'=>$row[3]);
+      $assoc = array('MailFrom'=>$row[0]);
       $return_v[] = $assoc;
     }
     
@@ -34,6 +43,31 @@ function getConnect($uid){
   ALERT("Fail to process the query.");
   return 0;
 }
+
+/*
+function getConnect($uid){
+  require_once "login2.php";
+  $conn = get_connection();
+  
+  $query = "SELECT User.Name as contact, Mail.Content, Mail.IdFrom, Mail.CreateAt, Mail.IdTo, Mail.Enabled
+            FROM User INNER JOIN Mail 
+            WHERE Mail.IdFrom=43 AND User.Name in(SELECT Name FROM User WHERE IdUser=Mail.IdTo) OR Mail.IdTo=43 AND User.Name in(SELECT Name FROM User WHERE IdUser=Mail.IdFrom)";
+  
+  if($result = $conn->query($query)){
+    $return_v = array();
+    while($row = $result->fetch_array()){
+      $assoc = array('MailFrom'=>$row[0], 'IdTo'=>$row[4], 'MailContent'=>$row[1], 'SentDate'=>explode(' ',$row[2])[0], 'Shows'=>$row[3]);
+      $return_v[] = $assoc;
+    }
+    
+    return $return_v;
+  }
+  
+  ALERT("Fail to process the query.");
+  return 0;
+}
+*/
+
 function ALERT($msg){
   echo "<script> alert('$msg'); </script>";
 }
